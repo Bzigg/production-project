9dbca5574b5aabdcb8e237d2d0c404f29f4752c3
no.plehanov <nek211097@yandex.ru>
2022-11-06 21:08:50 +0500

    storybook

---
+++ src/shared/ui/AppButton/AppButton.stories.tsx
@@ -0,0 +1,47 @@
+import React from "react"
+import { ComponentStory, ComponentMeta } from "@storybook/react"
+
+import { AppButton, ThemeButton } from "./AppButton"
+import { ThemeDecorator } from "shared/config/storybook/ThemeDecorator/ThemeDecorator"
+import { Theme } from "app/providers/ThemeProvider"
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+    title: "shared/AppButton",
+    component: AppButton,
+    // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+    argTypes: {
+        backgroundColor: { control: "color" },
+    },
+} as ComponentMeta<typeof AppButton>
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof AppButton> = (args) => (
+    <AppButton {...args} />
+)
+
+export const Primary = Template.bind({})
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Primary.args = {
+    children: "Text",
+}
+
+export const PrimaryDark = Template.bind({})
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+PrimaryDark.args = {
+    children: "Text",
+}
+
+PrimaryDark.decorators = [ThemeDecorator(Theme.DARK)]
+
+export const Clear = Template.bind({})
+Clear.args = {
+    children: "Text",
+    theme: ThemeButton.CLEAR,
+}
+
+export const Outline = Template.bind({})
+Outline.args = {
+    children: "Text",
+    theme: ThemeButton.OUTLINE,
+}
