5eb912050f7b13aec69070c232cd83308446e0c7
no.plehanov <nek211097@yandex.ru>
2022-11-06 17:23:41 +0500

    анализатор бандлов

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,5 +1,6 @@
 import HtmlWebpackPlugin from "html-webpack-plugin"
 import MiniCssExtractPlugin from "mini-css-extract-plugin"
+import { BundleAnalyzerPlugin } from "webpack-bundle-analyzer"
 import webpack, { web } from "webpack"
 import { BuildOptions } from "./types/config"
 
@@ -20,5 +21,8 @@ export function buildPlugins({
             __IS_DEV__: JSON.stringify(isDev),
         }),
         new webpack.HotModuleReplacementPlugin(),
+        new BundleAnalyzerPlugin({
+            openAnalyzer: false,
+        }),
     ]
 }
