5c70ccc2147743370e01cd2b20a509b5cc99d89e
no.plehanov <nek211097@yandex.ru>
2022-11-26 20:18:23 +0500

    модальное окно

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,15 +1,35 @@
-import { AppLink, AppLinkTheme } from "shared/ui/AppLink/AppLink"
 import { classNames } from "shared/lib/classNames/classNames"
 import cls from "./Navbar.module.scss"
+import { useCallback, useState } from "react"
+import { Modal } from "shared/ui/Modal/Modal"
+import { AppButton, ButtonTheme } from "shared/ui/AppButton/AppButton"
+import { useTranslation } from "react-i18next"
 
 interface NavbarProps {
     className?: string
 }
 
 export const Navbar = () => {
+    const [isAuthModal, setIsAuthModal] = useState(false)
+    const { t } = useTranslation()
+
+    const onToggleModal = useCallback(() => {
+        setIsAuthModal((prevValue) => !prevValue)
+    }, [])
+
     return (
         <div className={classNames(cls.Nav, {}, [])}>
-            <div></div>
+            <div>
+                <AppButton
+                    theme={ButtonTheme.CLEAR_INVERTED}
+                    onClick={onToggleModal}
+                >
+                    {t("Войти")}
+                </AppButton>
+            </div>
+            <Modal isOpen={isAuthModal} onClose={onToggleModal}>
+                123
+            </Modal>
         </div>
     )
 }
